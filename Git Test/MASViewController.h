8e0c488d2c5974322b44120464b7374cf978232c
MStuv <markstuver@MacBook-Pro.local>
2013-10-18 22:13:38 -0400

    Initial Commit

---
+++ Git Test/MASViewController.h
@@ -0,0 +1,13 @@
+//
+//  MASViewController.h
+//  Git Test
+//
+//  Created by Mark Stuver on 10/18/13.
+//  Copyright (c) 2013 Halo International Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MASViewController : UIViewController
+
+@end
