2490e47f04530cc3cb7007f4b266ccf38c357f8f
MStuv <markstuver@MacBook-Pro.local>
2013-10-19 00:20:51 -0400

    added a test to class

---
+++ MASTest.h
@@ -0,0 +1,13 @@
+//
+//  MASTest.h
+//  Git Test
+//
+//  Created by Mark Stuver on 10/19/13.
+//  Copyright (c) 2013 Halo International Corp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MASTest : NSObject
+
+@end
---
+++ MASTest.m
@@ -0,0 +1,13 @@
+//
+//  MASTest.m
+//  Git Test
+//
+//  Created by Mark Stuver on 10/19/13.
+//  Copyright (c) 2013 Halo International Corp. All rights reserved.
+//
+
+#import "MASTest.h"
+
+@implementation MASTest
+
+@end
